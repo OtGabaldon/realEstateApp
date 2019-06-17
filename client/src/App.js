1a4dfc4a18c4b5c4d158f3bf41e527a2289e52ca
Ot Gabaldon Torrents <gabaldonot@gmail.com>
2019-06-17 17:02:22 -0400

    Setting up App and bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+function App() {
+  return (
+    <div class="card" style={{ width: "18rem", margin: "auto" }}>
+      <div class="card-body">
+        <h5 class="card-title">Testing Bootstrap</h5>
+        <h6 class="card-subtitle mb-2 text-muted">So Boot</h6>
+        <p class="card-text">So Strap</p>
+        <a href="http://www.thisiswhy.biz" class="card-link">Check Out my website</a>
+      </div>
+    </div>
+  )
+}
+
+export default App;
